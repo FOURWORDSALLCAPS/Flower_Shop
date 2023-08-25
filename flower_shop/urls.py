1cf0ea860b62b38f825fddfe79d2b5bed7ba477c
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-08-25 11:17:02 +0300

    Add created order for user

--- flower_shop/urls.py
+++ flower_shop/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from flower_page.views import view_index, view_catalog, view_consultation, \
-    view_order, view_order_step, view_quiz, view_quiz_step, view_result, view_card
+    view_order, view_order_step, view_quiz, view_quiz_step, view_result, view_card, process_payment
 
 from . import settings
 
@@ -18,4 +18,5 @@ urlpatterns = [
     path('quiz_step/', view_quiz_step),
     path('result/', view_result),
     path('card/<int:card_id>', view_card),
+    path('process_payment/', process_payment, name='process_payment'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
